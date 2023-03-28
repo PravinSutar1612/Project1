a89b6a3cbb57ec3dcc381b75f84a1e0784db79f8
Pravin Sutar <128134416+PravinSutar1612@users.noreply.github.com>
2023-03-28 11:46:51 +0530

    Add files via upload

---
+++ ArrayExample3.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class ArrayExample3 {
+
+	public static void main(String[] args) {
+
+
+		float f[]= new float[12];
+		double d[]= new double[22];
+		char c[]=new char[22];
+			
+		char c1[]= {'n','e','h','a'};// datatype
+		String s="neha"; //class name= string
+		//wap to store five name 
+		String name[]= {"neha","ashu","gagan"};
+		}
+
+	}
+
+}
